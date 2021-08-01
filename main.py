1b3b4501d00b8fe39cc13a53f38fcfff46b1b043
BashirSbaiti <bashirsbaiti@gmail.com>
2021-07-31 20:02:56 -0400

    clean code, add functionality to administer therapeutic

---
+++ main.py
@@ -0,0 +1,373 @@
+import numpy as np
+from neuron import h
+from neuron.units import ms, mV
+import matplotlib.pyplot as plt
+import pprint
+from scipy.special import expit
+import network
+import random as rand
+import networkx as nx
+
+h.load_file("stdrun.hoc")
+h.load_file("import3d.hoc")
+
+
+class Cell:
+    def __init__(self, filename, x, y, z, theta):
+        if filename == "b&s":
+            self.BnS_morphology()
+            self.BnD_ephys()
+        else:
+            cell = h.Import3d_SWC_read()
+            cell.input(filename)
+            h.Import3d_GUI(cell, 0)
+            i3d = h.Import3d_GUI(cell, 0)
+            i3d.instantiate(self)
+            for sec in self.all:
+                sec.Ra = 100  # Axial resistance in Ohm * cm
+                sec.cm = 1  # Membrane capacitance in micro Farads / cm^2
+            self.soma[0].insert('hh')
+            for seg in self.soma[0]:
+                seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+                seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+                seg.hh.gl = 0.0003  # Leak conductance in S/cm2
+                seg.hh.el = -54.3  # Reversal potential in mV
+            for dend in self.dend:
+                dend.insert('pas')
+            for dend in self.dend:
+                for seg in dend:
+                    seg.pas.g = 0.001  # Passive conductance in S/cm2
+                    seg.pas.e = -65  # Leak reversal potential mV
+        self.x = self.y = self.z = 0
+        self.tauC = 0.0
+        self.isAlive = True
+        self.syn = h.ExpSyn(self.dend(0.5)) if type(self.dend) is not list else h.ExpSyn(self.dend[0](0.5))
+        self.syn.tau = 2 * ms
+        h.define_shape()
+        self.rotateZ(theta)
+        self.set_pos(x, y, z)
+        for sec in self.all:
+            sec.nseg = 1 + 10 * int(sec.L / 5)
+
+    def BnS_morphology(self):
+        """initialzes cell morphology"""
+        self.soma = h.Section(name='soma', cell=self)
+        self.dend = h.Section(name='dend', cell=self)
+        self.all = [self.soma, self.dend]
+        self.dend.connect(self.soma)
+        self.soma.L = self.soma.diam = 12.6157  # microns
+        self.dend.L = 200
+        self.dend.diam = 1
+
+    def BnD_ephys(self):
+        """initlizes cell biophysics"""  # TODO: compte IF curve (current v firing rate) / improve ephys?
+        for sec in self.all:
+            sec.Ra = 100  # Axial resistance in Ohm * cm
+            sec.cm = 1  # Membrane capacitance in micro Farads / cm^2
+        self.soma.insert('hh')
+        for seg in self.soma:
+            seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+            seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+            seg.hh.gl = 0.0003  # Leak conductance in S/cm2
+            seg.hh.el = -54.3  # Reversal potential in mV
+        # passive current
+        self.dend.insert('pas')
+        for seg in self.dend:
+            seg.pas.g = 0.001  # Passive conductance in S/cm2
+            seg.pas.e = -65  # Leak reversal potential mV
+
+    def set_pos(self, x, y, z):
+        """sets the position"""
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                sec.pt3dchange(i,
+                               x - self.x + sec.x3d(i),
+                               y - self.y + sec.y3d(i),
+                               z - self.z + sec.z3d(i),
+                               sec.diam3d(i))
+        self.x, self.y, self.z = x, y, z
+
+    def rotateZ(self, theta):
+        """Rotate the cell about the Z axis."""
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                x = sec.x3d(i)
+                y = sec.y3d(i)
+                c = h.cos(theta)
+                s = h.sin(theta)
+                xprime = x * c - y * s
+                yprime = x * s + y * c
+                sec.pt3dchange(i, xprime, yprime, sec.z3d(i), sec.diam3d(i))
+
+    def getInfo(self):
+        return f"cell at location ({self.x}, {self.y})"
+
+    def kill(self, lists=False):
+        for sec in self.all:
+            sec.Ra = float('inf')
+        for seg in self.soma[0] if lists else self.soma:
+            seg.hh.gnabar = 0
+            seg.hh.gkbar = 0
+            seg.hh.gl = 0
+        for dend in self.dend if lists else range(1):
+            for seg in dend if lists else self.dend:
+                seg.pas.g = 0
+        self.isAlive = False
+
+
+def createCells(matrix, r):
+    n = len(matrix)
+    cells = []
+    for x in range(n):
+        theta = x * 2 * h.PI / n
+        cells.append(Cell("b&s", h.cos(theta) * r, h.sin(theta) * r, 0, theta))
+    return cells
+
+
+def connectCells(matrix, cells, verbose=False):
+    """:returns adj matrix of netcons, [from][to]"""
+    n = len(matrix)
+    ncs = np.zeros((n, n), dtype=object)
+    for i, cell in enumerate(cells):
+        connectTo = [tgti for tgti, x in enumerate(matrix[i]) if x == 1]
+        for index in connectTo:
+            tgt = cells[index]
+            nc = h.NetCon(cell.soma(0.8)._ref_v, tgt.syn, sec=cell.soma) if type(cell.soma) is not list else h.NetCon(
+                cell.soma[0](0.8)._ref_v, tgt.syn, sec=cell.soma[0])
+            # print(f"{nc} has originates cell {nc.precell()} and targets cell {nc.postcell()}")
+            nc.weight[0] = .05
+            nc.delay = 5
+            ncs[i][index] = nc
+            if verbose:
+                print(f"Connected cell {i} to cell {index}.")
+    return ncs
+
+
+def countSpikes(vector):
+    v = list(vector)
+    c = 0
+    for val in v:
+        if val > 10:
+            c += 1
+    return c
+
+
+def showStruc():
+    ps = h.PlotShape(True)
+    ps.show(0)
+    input()
+
+
+def getSpikeCounts(matrix, spikeTimeVecs, tstart, tstop):
+    """returns a matrix shape=(i,j) of spike counts for cell i firing to cell j in the interval (tstart, tstop)
+    will also copy firing times of cell a to cell b to all of the cells cell a fires too, because neuron
+    will only keep track of one"""
+    n = len(matrix)
+    firingMat = np.zeros_like(matrix)
+    for fro in range(n):
+        for to in range(n):
+            spikeList = list(spikeTimeVecs[fro][to])
+            # if h.t <=199:
+            # print(spikeList, h.t, tstart, tstop)
+            spikeListInInterval = [el for el in spikeList if tstart < el < tstop]
+            if len(spikeListInInterval) > 0:
+                firingMat[fro] = np.array(matrix[fro]) * len(spikeListInInterval)
+    return firingMat
+
+
+def seedTau(cells, initial_frac=0.1, intial_conc=0.1):
+    n = len(cells)
+    toInfect = int(initial_frac * n)
+    infectedCount = 0
+
+    while infectedCount < toInfect:
+        rIndex = int(rand.random() * n)
+        if cells[rIndex].tauC == 0:
+            cells[rIndex].tauC = intial_conc
+            infectedCount += 1
+
+
+def propogateTau(cells, FRMatrix, verbose=False):
+    for i, cell in enumerate(cells):
+        incidentCells = np.array([ind for ind, x in enumerate(FRMatrix[:, i]) if x > 0])
+        incidentFRs = np.array([x for x in FRMatrix[:, i] if x > 0])
+        incidentPercentFiringAc = 0 if np.sum(incidentFRs) == 0 else incidentFRs / np.sum(incidentFRs)
+        incidentTaus = np.zeros(len(incidentCells))
+        for ind, i2 in enumerate(incidentCells):
+            incidentTaus[ind] = cells[i2].tauC
+        dTau = np.sum(incidentPercentFiringAc * incidentTaus)
+        if verbose:
+            # print(FRMatrix)
+            print(f"{incidentTaus}\t{incidentFRs}\t{dTau} for cell {i} which currently has tau = {cell.tauC}")
+        cell.tauC = sigmoid(cell.tauC + dTau) if dTau > 0 else cell.tauC
+
+
+def sigmoid(x):
+    return expit(x)
+
+
+net = network.small_world(20, 2, 0)
+# net = network.scale_free(10, 2)
+# network.visualize_network(net)
+# plt.show()
+matrix = nx.to_numpy_array(net)
+print(f"Network has {np.sum(matrix)} total connections")
+cells = createCells(matrix, r=600)
+netcons = connectCells(matrix, cells, verbose=False)
+# showStruc()
+n = len(matrix)
+
+
+def killCellsAbove(threshold, verbose=False):
+    for i, cell in enumerate(cells):
+        # print(f"cell {i} has tauC = {cell.tauC}, time = {h.t}")
+        if cell.tauC > threshold and cell.isAlive:
+            cell.kill()
+            if verbose:
+                print(f"killed cell {i} at {h.t}")
+
+
+def deliverTherapeutic(TauReducFactor, prop, verbose=False):
+    """reduced tau concentration by TauReducFactor (percent) in prop percent of infected cells"""
+    threshold = 0.0001  # concentration that is close enough to zero
+    n = len(cells)
+    numInfectedCells = len([cell for cell in cells if cell.tauC > threshold and cell.isAlive])
+    numCellsToAffect = prop * numInfectedCells
+    numAffected = 0
+
+    while numAffected < numCellsToAffect:
+        rIndex = int(rand.random() * n)
+        # if it was > 0, would go on forever
+        if cells[rIndex].tauC > threshold and cells[rIndex].isAlive:  # TODO affect dead cells?
+            cells[rIndex].tauC -= TauReducFactor * cells[rIndex].tauC
+            numAffected += 1
+            if verbose:
+                print(f"cell {rIndex}'s tau concentration reduced to {cells[rIndex].tauC}")
+
+
+def initialStim(number, start, delay, weight, tau):
+    stim = h.NetStim()
+    syn = h.ExpSyn(cells[n // 2].dend(0.8)) if type(cells[n // 2].dend) is not list else h.ExpSyn(
+        cells[n // 2].dend[0](0.8))
+    stim.number = number
+    stim.start = start
+
+    ncstim = h.NetCon(stim, syn)
+    ncstim.delay = delay * ms
+    ncstim.weight[0] = weight
+
+    syn.tau = tau * ms
+
+    return ncstim, stim, syn
+
+
+def initSim(Vm, tstop):
+    h.finitialize(Vm * mV)
+    # note: events are cleared at initialization
+    events = list()
+    for ts in range(40):
+        events.append(h.CVode().event(ts * 5, step))
+    h.continuerun(tstop * ms)
+
+
+ncstim, stim, syn = initialStim(5, 9, 1, 0.08, 2 * ms)
+
+
+def recordV(cellNumber, region="soma"):
+    if region == "soma":
+        return h.Vector().record(
+            cells[cellNumber].soma(0.5)._ref_v if type(cells[cellNumber].soma) is not list
+            else cells[cellNumber].soma[0](0.5)._ref_v)
+    else:
+        return h.Vector().record(
+            cells[cellNumber].dend(0.5)._ref_v if type(cells[cellNumber].dend) is not list
+            else cells[cellNumber].dend[0](0.5)._ref_v)
+
+
+v = recordV(0, region="soma")
+t = h.Vector().record(h._ref_t)
+
+spikeTimeMat_Unexpanded = np.zeros((n, n), dtype=object)
+for fro in range(n):
+    for to in range(n):
+        spikeTimeMat_Unexpanded[fro, to] = h.Vector()
+        if netcons[fro, to] != 0:
+            netcons[fro, to].record(spikeTimeMat_Unexpanded[fro, to])
+
+seedTau(cells, initial_frac=.2, intial_conc=.1)
+tauMatrix = []
+
+
+def step(t=0, dt=5, verbose=False):
+    # h(f'StepBy={dt}')  # ms
+    # h('walltime = startsw()')
+    # h.xopen("midbalfcn.hoc")
+    # h('objref fihw')
+    # h('fihw = new FInitializeHandler(2, "midbal()")')
+    spikeTimes = getSpikeCounts(matrix, spikeTimeMat_Unexpanded, h.t - dt, h.t)
+    if verbose:
+        print(f"spikes in interval = {np.sum(spikeTimes)}")
+    propogateTau(cells, spikeTimes)
+    taus = np.array([cell.tauC for cell in cells]).reshape(n, 1)
+    global tauMatrix
+    if len(tauMatrix) == 0:
+        tauMatrix = taus
+    else:
+        tauMatrix = np.hstack((tauMatrix, taus))
+    killCellsAbove(.8, verbose=False)
+    deliverTherapeutic(.2, .2, verbose=False)
+
+
+initSim(-65, 200 * ms)
+timeSteps = 40
+dt = 5
+
+# step(ts * dt, dt)
+
+
+def plotTauPrev():
+    fig = plt.figure()
+    plt.title("Average Cellular Tau over Time")
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Average Cellular Tau (0-1)')
+    tauPrev = np.sum(tauMatrix, axis=0) / n
+    plt.plot(np.arange(0, timeSteps, 1)*dt, tauPrev)
+    plt.show()
+
+
+plotTauPrev()
+
+
+def plotSpikeTimes(plotType):
+    fig, ax = plt.subplots()
+    plt.title("Spike Times of Cells in Network")
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Cell Number')
+
+    for fro in range(n):
+        for i, spike_times_vec in enumerate(spikeTimeMat_Unexpanded[fro]):
+            X = list(spike_times_vec)
+            if len(X) == 0:
+                continue
+            if plotType == "raster":
+                plt.vlines(X, fro - .5, fro + .5)
+            elif plotType == "scatter":
+                Y = fro * np.ones_like(X)
+                ax.scatter(X, Y, color='k')
+            else:
+                return
+    plt.show()
+
+
+def plotMemPotential(cellNumber):
+    plt.figure()
+    plt.title(f"Membrane Potential of Cell {cellNumber} Over Time")
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Membrane Potential (mV)')
+    plt.plot(t, v)
+    plt.show()
+
+
+plotSpikeTimes("raster")
+plotMemPotential(0)
+
